d1a2b01890817a89a9057e0140ab9557e181fea0
TiNTen <57715701+TiNTen-coder@users.noreply.github.com>
2021-04-29 21:10:57 +0300

    pre-alpha v.1002

--- main.py
+++ main.py
@@ -323,8 +323,8 @@ def main():
     api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
 
     port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
+    #app.run(host='127.0.0.1', port=port)
 
 
 if __name__ == '__main__':
